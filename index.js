cce7829b882c4a3b46e255d0fce6cfef6f499c56
Noor <noormir1295@gmail.com>
2020-09-21 16:37:45 -0400

    adding connection

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const mysql = require("mysql");
 const inquirer=require("inquirer");
 const consoleTable=require("console.table");
+
+var connection = mysql.createConnection({
+  ost: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "employee_management_systemDB",
+});
